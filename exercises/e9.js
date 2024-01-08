6b13ad5e747da829efe906ab95913f3a8dd2568b
Andre Ansah <145819692+PAPERPL8S@users.noreply.github.com>
2024-01-08 00:02:42 -0600

    completed e9.js

--- exercises/e9.js
+++ exercises/e9.js
@@ -6,10 +6,25 @@ import { data } from "../data/data";
 
 export function getEarthData(data) {
   // Your code goes here...
+ const earthData = data['Earth'];
+  return earthData ? earthData : {
+    "avgTemp": 288,
+    "gravity": 9.8,
+    "id": "earth",
+    "isPlanet": true,
+    "mass": {
+      "massExponent": 24,
+      "massValue": 5.97237,
+    },
+    "moons": ["La Lune"],
+    "moonsCount": 1,
+    "name": "Earth",
+    "vol": {
+      "volExponent": 12,
+      "volValue": 1.08321,
+    }
+  };
 }
-
-
-
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-9"
 // If the test has all tests passed, switch to the next exercise file
