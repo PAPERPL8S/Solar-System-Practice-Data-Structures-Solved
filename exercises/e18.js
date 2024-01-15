f645a229cb6226c0cc18df16f3874ea5ce4660f1
Andre Ansah <145819692+PAPERPL8S@users.noreply.github.com>
2024-01-14 22:57:26 -0600

    completed e18.js

--- exercises/e18.js
+++ exercises/e18.js
@@ -7,8 +7,21 @@
 export function getGreatestDiscoveryYear(data) {
   // Your code goes here...
   // feel free to import your `maxBy` or `minBy` methods from previous lessons
+    
+  if (data.asteroids.length === 0) {
+    return undefined;
+  }
+
+  const yearsMap = data.asteroids.reduce((map, asteroid) => {
+    const year = asteroid.discoveryYear;
+    map.set(year, (map.get(year) || 0) + 1);
+    return map;
+  }, new Map());
+
+  return [...yearsMap.entries()].reduce((max, entry) => (entry[1] > max[1] ? entry : max), [undefined, 0])[0];
 }
 
+  
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-18"
 // If the test has all tests passed, switch to the next exercise file
