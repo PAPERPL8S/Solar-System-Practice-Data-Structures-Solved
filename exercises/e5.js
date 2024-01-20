78ad195349dfc297959eb5c0484b5a2c28a7ba2e
Andre Ansah <145819692+PAPERPL8S@users.noreply.github.com>
2024-01-20 13:27:35 -0600

    completed e5 & e6.js

--- exercises/e5.js
+++ exercises/e5.js
@@ -6,22 +6,13 @@ import { data } from "../data/data";
 
 export function getPlanetsWithMassValue(data, number) {
   // Your code goes here...
-  
-  const planets = [
-    { name: 'Mercure', massValue: 3.30114 },
-    { name: 'Uranus', massValue: 8.68127 },
-    { name: 'Mars', massValue: 6.41712 },
-    { name: 'Jupiter', massValue: 1.89819 },
-    { name: 'Saturne', massValue: 5.68336 },
-    { name: 'Earth', massValue: 5.97237 },
-    { name: 'Vénus', massValue: 4.86747 },
-    { name: 'Neptune', massValue: 1.02413 }
-  ];
-
-  return planets.filter(planet => planet.massValue >= number).map(planet => planet.name);
+  return data.planets
+    .filter((planet) => planet.mass && planet.mass.massValue >= number)
+    .map((planet) => planet.name);
 }
-
+ 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-5"
 // If the test has all tests passed, switch to the next exercise file
 // If any of the tests fails, refactor the code and run the test command after you've fixed the function
+// 
