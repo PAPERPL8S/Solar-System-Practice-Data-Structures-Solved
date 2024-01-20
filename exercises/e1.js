fc1589435e38b8e6413e566a823c070c5f2f42bb
Andre Ansah <145819692+PAPERPL8S@users.noreply.github.com>
2024-01-20 11:43:08 -0600

    completed e1.js

--- exercises/e1.js
+++ exercises/e1.js
@@ -6,10 +6,7 @@ import { data } from "../data/data";
 
 export function getPlanetNames(data) {
   // Your code goes here...
-  const allPlanets = data.planets;
-  const planetNames = Object.values(allPlanets).map(planet => planet.name);
-  
-  return planetNames;
+  return data.planets.map((planet) => planet.name);
 }
 
 
